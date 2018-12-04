1d2eadea1c04d23100391632dd7a5cb8fcfb4352
Beksultan Galiev <inferion.frost@gmail.com>
2018-12-04 22:23:06 +0600

    version 0.0.8: added XRoadSecurityServerIdentifierType; removed useless try catch clauses with printStackTrace (throw exception instead);

--- xroad-spring-core/src/main/java/kg/gov/tunduk/xroad/XRoadXsdSchemaTypesProvider.java
+++ xroad-spring-core/src/main/java/kg/gov/tunduk/xroad/XRoadXsdSchemaTypesProvider.java
@@ -1,5 +1,6 @@
 package kg.gov.tunduk.xroad;
 
+import com.ibm.wsdl.ImportImpl;
 import org.springframework.ws.wsdl.wsdl11.provider.InliningXsdSchemaTypesProvider;
 
 import javax.wsdl.Definition;
@@ -9,6 +10,12 @@ public class XRoadXsdSchemaTypesProvider extends InliningXsdSchemaTypesProvider 
 
     @Override
     public void addTypes(Definition definition) throws WSDLException {
+        // Importing xroad.xsd
+        // <xs:import namespace="http://x-road.eu/xsd/xroad.xsd" schemaLocation="http://x-road.eu/xsd/xroad.xsd"/>
+        ImportImpl xRoadXsdImport = new ImportImpl();
+        xRoadXsdImport.setLocationURI("http://x-road.eu/xsd/xroad.xsd");
+        xRoadXsdImport.setNamespaceURI("http://x-road.eu/xsd/xroad.xsd");
+        definition.addImport(xRoadXsdImport);
         super.addTypes(definition);
     }
 }
